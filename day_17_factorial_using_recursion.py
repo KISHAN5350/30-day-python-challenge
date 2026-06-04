03280cb964198a1c2ac3793d6b625bf845ec7fc3
KISHAN5350 <kishankr041@gmail.com>
2026-06-04 09:15:52 +0530

    finding_factorial_using_recursion

---
+++ day_17_factorial_using_recursion.py
@@ -0,0 +1,14 @@
+
+
+# Experiment 40: Factorial Using Recursion
+
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+
+print(f"Factorial of {num} = {factorial(num)}")
