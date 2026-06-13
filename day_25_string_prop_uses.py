e91b770f310e10e969befc76a906c5147e403c03
KISHAN5350 <kishankr041@gmail.com>
2026-06-13 19:50:25 +0530

    basic_string_prop

---
+++ day_25_string_prop_uses.py
@@ -0,0 +1,12 @@
+
+
+# Experiment 44: String Creation, Concatenation and Substring
+
+s1 = input("Enter first string : ")
+s2 = input("Enter second string: ")
+
+concat = s1 + s2
+
+print("Concatenated :", concat)
+print("Substring [0:5]:", concat[0:5])
+print("Length :", len(concat))
