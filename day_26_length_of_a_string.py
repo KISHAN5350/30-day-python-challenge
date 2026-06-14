dc0c25eb7743ad1b31951112877130de8fec97ac
KISHAN5350 <kishankr041@gmail.com>
2026-06-14 20:05:41 +0530

    without_lib_function

---
+++ day_26_length_of_a_string.py
@@ -0,0 +1,13 @@
+
+
+# Experiment 45: Length of a String Without Library Function
+
+s = input("Enter a string: ")
+
+count = 0
+
+for ch in s:
+    count += 1
+
+print("String :", s)
+print("Length :", count)
